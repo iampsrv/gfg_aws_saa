287d568f916be1e951e29c1b405ff098c97459e3
iampsrv <iampsrv@gmail.com>
2024-09-21 14:01:47 +0530

    week 5 res

--- Week 4/app/app.py
+++ Week 4/app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-        return f'Hello World v3'
+        return f'Hello World'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0',port=5001,debug=True)
